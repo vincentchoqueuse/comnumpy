e434e8151920e98c8f0e600fe784423afb0c6c5b
alexandrufrunza <alexandru.ionut.frunza@gmail.com>
2026-01-28 13:48:25 +0200

    Updates

--- src/comnumpy/optical/pdm/utils.py
+++ src/comnumpy/optical/pdm/utils.py
@@ -1,6 +1,13 @@
 from dataclasses import dataclass
 import numpy as np
 from scipy.stats import maxwell
+import pathlib
+import pandas as pd
+
+from comnumpy.core.generics import Processor
+from comnumpy.core.utils import hard_projector
+
+
 
 
 def build_pmd_segments(L_km: float, D_pmd_ps_sqrt_km: float, N_segments: int) -> tuple[np.ndarray, np.ndarray]:
@@ -42,3 +49,89 @@ def build_pmd_segments(L_km: float, D_pmd_ps_sqrt_km: float, N_segments: int) ->
     theta_k = np.random.uniform(0.0, np.pi, size=N_segments)
 
     return tau_k, theta_k
+
+
+@dataclass
+class DifferentialEncoding(Processor):
+    modulation:str
+    order:int
+    norm:bool = True
+
+    def get_binary_repr(self):
+        pathname = pathlib.Path("src/comnumpy/core/data")
+        filename = "{}/{}_{}_bin.csv".format(pathname, self.modulation, self.order)
+        data = pd.read_csv(filename, dtype={'bin':str})
+        bin_list = data['bin'].to_numpy().reshape(-1,1)
+        return bin_list
+    
+    def diff_encode(self, data_bin):
+        # 1. Define the Gray-coded positions for the first quadrant
+        # bits (b3, b4) -> complex position
+        first_quad_map = {
+        (0,0): 1+1j,
+        (0,1): 1+3j,
+        (1,1): 3+3j,
+        (1,0): 3+1j
+        }
+
+        # 2. Differential Mapping for (b1, b2) -> Quadrant Shift
+        # Using Gray-like mapping for shifts too: 00=0, 01=1, 11=2, 10=3
+        diff_map = {(0,0): 0, (0,1): 1, (1,1): 2, (1,0): 3}
+        symbols = np.zeros(len(data_bin), dtype=complex)
+        state_q = 0 # Current quadrant state
+        for i in range(len(data_bin)):
+            # Get data_bin
+            b_diff = tuple(map(int, data_bin[i][0:2]))
+
+            b_gray = tuple(map(int, data_bin[i][2:]))
+            # Update quadrant state (Differential)
+            shift = diff_map[b_diff]
+            state_q = (state_q + shift) % 4
+            
+            # Get base symbol (Gray)
+            base_sym = first_quad_map[b_gray]
+            
+            # Rotate base symbol to the current quadrant
+            # 0 -> *1, 1 -> *j, 2 -> *-1, 3 -> *-j
+            symbols[i] = base_sym * np.exp(1j*state_q*np.pi/2)
+        return symbols
+
+    
+    def forward(self, x):
+        bin_repr = self.get_binary_repr()
+        data_bin = bin_repr[x].ravel()
+        diff_symb = self.diff_encode(data_bin)
+        if self.norm:
+            encoded_symb = diff_symb/np.sqrt(np.mean(np.abs(diff_symb)**2))
+        return encoded_symb
+
+@dataclass
+class Differential_Decoding(Processor):
+    modulation: str
+    order: int
+
+    def forward(self, x):
+        x *= np.exp(1j*2.5*np.pi/2)
+        prev = x[0]
+        theta = 0
+        decoded = np.zeros_like(x)
+
+        for k in range(1, len(x)):
+            z = x[k] * np.conj(prev)
+
+            # 1. detect quadrant (differential)
+            angle = np.angle(z)
+            q = int(np.round(angle / (np.pi/2))) % 4
+            phi_hat = q * (np.pi/2)
+
+            # update cumulative estimated phase
+            theta += phi_hat
+
+            # 2. remove estimated phase
+            y = x[k] * np.exp(-1j * theta)
+
+            decoded[k] = y
+            print(x[k], decoded[k], sep='\n')
+            prev = x[k]
+
+        return decoded
---
+++ src/comnumpy/optical/pdm/validation/farsi_2022.py
@@ -0,0 +1,208 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import pandas as pd
+from matplotlib.ticker import NullFormatter, LogFormatterMathtext
+
+import sys
+sys.path.insert(0, "src\\")
+
+from comnumpy.core import Sequential
+from comnumpy.core.channels import AWGN
+from comnumpy.core.generators import SymbolGenerator
+from comnumpy.core.mappers import SymbolMapper, SymbolDemapper
+from comnumpy.core.utils import get_alphabet, hard_projector
+from comnumpy.optical.pdm.sinks import IQ_Scope, IQ_Scope_PostProcessing
+from comnumpy.core.filters import SRRCFilter
+from comnumpy.core.processors import Upsampler, Downsampler
+from comnumpy.core.monitors import Recorder
+from comnumpy.core.metrics import compute_ser, compute_ber, compute_evm
+
+from comnumpy.optical.pdm.generics import PDMWrapper, ChannelWrapper
+from comnumpy.optical.pdm.channels import SOP, PDL
+from comnumpy.optical.pdm.compensators import CMA, RDE, DDLMS, AdaptiveChannel, Switch, MCMA
+from comnumpy.optical.pdm.utils import *
+
+# parameters
+type, M = "QAM", 16
+N = 1_200_000
+alphabet = get_alphabet(type, M, type='bin')
+
+Fs = 56e9 
+Ts = 1 / 28e9
+
+seg = 1 # number of fiber segments
+
+# SRRC filter
+roll_off = 0.35
+srrc_taps = 30
+
+# Oversampling 
+oversampling = 2
+
+### Impairments parameters ###
+# SOP drift
+pol_linewidth = 1.4e4
+
+#PDL parameters
+pdl_db = 3
+pdl_theta = np.pi/4
+
+Fiber_length = 500 # Km
+D_pmd = 0.1e-12 # ps/sqrt(km), for aggresive cases, choose 0.1
+t_dgd_k, rot_angle_k = build_pmd_segments(Fiber_length, D_pmd, seg)
+pmd_params = [t_dgd_k, rot_angle_k]
+
+P_total = 0 # in dB
+p_seg = P_total / np.sqrt(seg)
+pdl_params = np.random.uniform(0, np.sqrt(3)*p_seg, seg )
+
+# CMA #
+cma_taps = 16
+
+# CMA convergence #
+conv = 500_000
+
+tx = Sequential( [
+            Upsampler(oversampling),
+            SRRCFilter(roll_off, oversampling, srrc_taps, method='fft')
+] )
+
+
+channel = Sequential( [
+            #PDL(pdl_db, pdl_theta),
+            SOP(T_symb=Ts, linewidth=pol_linewidth),
+] )
+
+rx = Sequential( [
+            Downsampler(oversampling),
+] )
+
+channel_params = [ pol_linewidth, pmd_params, pdl_params ]
+step_CMA_list = np.logspace(-3, -3, num = 1)
+
+recorder_before_CMA = Recorder(name='data_before_CMA')
+recorder_emision = Recorder(name='tx_symbols_emision')
+recorder_real_symb = Recorder(name='tx_symbols')
+recorder_reception = Recorder(name='rx_symbols')
+
+SNR = 18
+chain = Sequential([
+            SymbolGenerator(M),
+            Recorder(name='data_tx'),
+            #PDMWrapper(SymbolMapper(alphabet)),
+            PDMWrapper(DifferentialEncoding('QAM', 16)),
+            PDMWrapper(IQ_Scope(axis='equal', nlim=(-10000,N))),
+            recorder_real_symb,
+            PDMWrapper(tx, 'tx'),
+            recorder_emision,
+            #ChannelWrapper(seq_obj=channel, L=seg, params=channel_params),
+            #PDMWrapper( AWGN(value=SNR, unit='snr_dB'), name='noise'),
+            PDMWrapper( SRRCFilter(roll_off, oversampling, srrc_taps, method='fft') ),
+            recorder_before_CMA,
+            #PDMWrapper(IQ_Scope_PostProcessing(axis='equal', nlim=(-10000,N))),
+            #MCMA(alphabet=alphabet, mu=1e-3, p=2, name="MCMA"),
+            #CMA(L=cma_taps, alphabet=alphabet, mu=1e-3, oversampling=oversampling, name='CMA'),
+            # PDM_Wrapper(IQ_Scope(axis='equal', nlim=(-10000,N))),
+            # Switch(cma_taps, alphabet, step_MIMO_list, oversampling, tx_before_CMA=recorder_before_CMA, name='adaptive_channel'),
+            #PDMWrapper(IQ_Scope(axis='equal', nlim=(-10000,N))),
+            PDMWrapper(rx,'rx'),
+            recorder_reception,
+            # MSE(recorder_real_symb, alphabet, name='mse'),
+            # PhUnGrid(None, name='PhUnCor'),
+            PDMWrapper(IQ_Scope(axis='equal', nlim=(-10000,N))),
+            # PDMWrapper(IQ_Scope_PostProcessing(axis='equal', nlim=(-10000,N))),
+            PDMWrapper( Differential_Decoding('QAM', 16) ),
+            Recorder(name='data_rx'),
+            ])
+
+
+####### 1 iteration ######
+
+y = chain(N)[:,conv:]
+data_tx = chain['data_tx'].get_data()
+data_tx1 = np.reshape(data_tx, (2,-1))[0,conv:]
+data_tx2 = np.reshape(data_tx, (2,-1))[1,conv:]
+
+ser1  = compute_ser(data_tx1, y[0,:])
+# ber1 = compute_ber( data_tx1, y[0,:], width=int(np.log2(M)) )
+ser2  = compute_ser(data_tx2, y[1,:])
+# ber2 = compute_ber( data_tx2, y[1,:], width=int(np.log2(M)) )
+ser = np.mean( [ser1, ser2] )
+# ber = np.mean( [ber1, ber2] )
+# print(f'SNR={SNR}:', 'SER:', ser1, '\tBER:', ber1)
+# print(f'SNR={SNR}:', 'SER:', ser2, '\tBER:', ber2)
+print(f'SNR={SNR}:', 'SER:', ser1)
+print(f'SNR={SNR}:', 'SER:', ser2)
+plt.show()
+
+
+# ####### Monte Carlo ######
+# ber_vs_step = []
+# nr_repetitions = 1
+# avg_ber_per_step = []
+
+# for mu in step_MIMO_list:
+#     # chain["CMA"].mu = mu
+#     # chain["sop_compensator"].mu = mu
+#     chain["MCMA"].mu = mu
+#     ber_runs = []
+
+#     for _ in range(nr_repetitions):
+#         # chain["CMA"].reset()
+#         # chain["sop_compensator"].reset()
+#         chain["MCMA"].reset()
+
+#         # reset ChannelWrapper
+#         for idx, mod in enumerate(chain.module_list):
+#             if isinstance(mod, ChannelWrapper):
+#                 chain.module_list[idx] = ChannelWrapper(
+#                     seq_obj=channel,
+#                     L=seg,
+#                     params=channel_params
+#                 )
+#                 break
+
+#         y = chain(N)[:, conv:]
+#         data_tx = chain['data_tx'].get_data()
+
+#         tx1 = np.reshape(data_tx, (2, -1))[0, conv:]
+#         tx2 = np.reshape(data_tx, (2, -1))[1, conv:]
+
+#         ber1 = compute_ber(tx1, y[0], width=int(np.log2(M)))
+#         ber2 = compute_ber(tx2, y[1], width=int(np.log2(M)))
+
+#         ber_runs.append(np.mean([ber1, ber2]))
+
+#     avg_ber = np.mean(ber_runs)
+#     avg_ber_per_step.append(avg_ber)
+    
+#     #csv
+#     ber_vs_step.append({
+#         "step": mu,
+#         "BER":avg_ber
+#     })
+#     print(f"mu={mu:.3e} → BER={avg_ber_per_step[-1]:.3e}")
+
+
+# #save the datas in a csv file
+# df = pd.DataFrame(ber_vs_step)
+# df.to_csv(f"dates_{seg}_seg_{nr_repetitions}reps_2_SOP_MCMA_16qam.csv", index=False)
+
+# plt.figure(figsize=(7, 5))
+# plt.loglog(step_MIMO_list, avg_ber_per_step, marker='o', linewidth=2)
+
+# plt.xlabel("SOP compensator step (μ)")
+# plt.ylabel("Average BER over Monte Carlo")
+# plt.title(f"BER vs STEP (SNR={SNR} dB)")
+
+# plt.grid(True, which='both', linestyle='--', linewidth=0.5)
+
+# ax = plt.gca()
+# ax.xaxis.set_major_formatter(LogFormatterMathtext())
+# ax.xaxis.set_minor_formatter(NullFormatter())
+# ax.yaxis.set_major_formatter(LogFormatterMathtext())
+# ax.yaxis.set_minor_formatter(NullFormatter())
+
+# plt.tight_layout()
+# plt.show()
